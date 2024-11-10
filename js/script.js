e4d69de7238e1ef568677ec9b9202f0cbad2fd97
Diego Hernández <108566267+diegocastilloho@users.noreply.github.com>
2024-11-10 11:12:11 -0600

    Subida de archivos

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// JavaScript Document
+
+
+
+$(document).ready(function () {	
+	
+
+$('.bot, .descargable').mouseover(function(){
+  gsap.fromTo(this, {opacity:0.5, scale:0.8}, { opacity:1 , scale:1})
+});	
+
+	
+
+
+	
+	
+	
+	
+	
+ 
+ });
+ 
+ 
+
